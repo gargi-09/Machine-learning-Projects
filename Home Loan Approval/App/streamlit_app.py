6013aa968463245df045f809acf0b727a1dd6086
Gargi Vipat <gargivipat2002@gmail.com>
2023-10-26 20:20:49 +0530

    Added data and streamlit app #2

---
+++ Home Loan Approval/App/streamlit_app.py
@@ -0,0 +1,12 @@
+# UI for Home loan approval project using streamlit
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import joblib
+model = joblib.load(open("classifier_model.joblib", "rb"))
+def predict_func(doc):
+    results = model.predict([doc])
+    return results
+def main():
+    st.title("Dream Housing Finance")
